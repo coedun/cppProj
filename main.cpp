8d41c78860f17108a707ac30958d5adf878650a4
coedun <coedun1@gmail.com>
2024-12-05 14:48:41 +0000

    standard input and output

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 int main() {
 	using namespace std;
 	//std::cout << "Hello " << "C++" << std::endl;
-	cout << "HelloWorld\n" << 45 << " " << 8.2f << endl;
+	/*cout << "HelloWorld\n" << 45 << " " << 8.2f << endl;
 	int age;
 	cout << "Tell me your age: ";
 	cin >> age;
-	cout << "Your age is: " << age << endl;
+	cout << "Your age is: " << age << endl;*/
 	char buff[512];
 	cout << "What's your name? ";
-	cin >> buff;
+	//cin >> buff;
+	cin.getline(buff, 64, '\n');
 	cout << "Your name is : " << buff << endl;
 	return 0;
 }
