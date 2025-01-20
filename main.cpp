14a0441463926cd26c33d0a5c285a64cc7a8df1a
coedun <coedun1@gmail.com>
2025-01-20 07:55:11 +0000

    Using References in C++

--- main.cpp
+++ main.cpp
@@ -2,16 +2,11 @@
 
 int main() {
 	using namespace std;
-	//std::cout << "Hello " << "C++" << std::endl;
-	/*cout << "HelloWorld\n" << 45 << " " << 8.2f << endl;
-	int age;
-	cout << "Tell me your age: ";
-	cin >> age;
-	cout << "Your age is: " << age << endl;*/
-	char buff[512];
-	cout << "What's your name? ";
-	//cin >> buff;
-	cin.getline(buff, 64, '\n');
-	cout << "Your name is : " << buff << endl;
+	int x = 10;
+
+	int& ref = x;
+	ref = 6;
+	cout << "x:" << x << endl;
+	cout << "ref: " << ref << endl;
 	return 0;
 }
